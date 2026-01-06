b8ad58d9f7b3668cf3fcb696ed4b3b738817180a
NabilApipp <nabilapipp@gmail.com>
2026-01-06 14:46:47 +0700

    fixing style color & font hero component

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -5,10 +5,10 @@ import { TypeAnimation } from "react-type-animation";
 export default function Hero() {
   return (
     <>
-      <div className="pl-10 pr-10 min-h-screen flex" id="home">
+      <div className="pl-10 pr-10 min-h-screen flex pt-16" id="home">
         {/* Container 1 */}
         <div className="md:flex md:flex-col flex-row  gap-8 justify-center w-1/2">
-          <p className="text-4xl">Hey, I'm</p>
+          <p className="text-4xl font-poppins font-semibold">Hey, I'm</p>
           <h1 className="font-lolita text-6xl">
             <TypeAnimation
               sequence={["Nabil Arif Triyanto", 2000]}
@@ -17,7 +17,7 @@ export default function Hero() {
               repeat={Infinity}
             />
           </h1>
-          <p className="font-poppins text-[1rem]">
+          <p className="font-poppins font-semibold text-[1rem]">
             Passionate about <span>Website Development. </span>I love creating
             innovative solutions using modern technologies like
             <span>React, Next.js, TypeScript, and more.</span> Currently focused
@@ -26,10 +26,10 @@ export default function Hero() {
           </p>
         </div>
         {/* Container 2 */}
-        <div className="flex justify-center items-center w-1/2">
+        <div className="flex items-center justify-center w-1/2">
         {/* Personal Image */}
           <img
-            src="asset\image\personal\personal_photo.jpg"
+            src="asset\image\personal\personal_phot.jpg"
             alt="Image Profile"
             className="w-auto h-[500px] rounded"
           />
