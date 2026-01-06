2e2bd752b68f198a13bcb6bd2252ab5b23f682fb
NabilApipp <nabilapipp@gmail.com>
2026-01-06 14:20:33 +0700

    fixing style edcuation

--- app/components/education.tsx
+++ app/components/education.tsx
@@ -13,7 +13,7 @@ const edcuation: Education[] = [
     scholl_name: "SMKN 4 Kendal",
     location: "Kendal, Central Java",
     dateRange: "Jan 2024 - Present",
-    desc: "High Scholl - RPL (Rekayasa Perangkat Lunak)"
+    desc: "High School - RPL (Rekayasa Perangkat Lunak)"
   },
 ];
 
@@ -28,7 +28,7 @@ function DashItem({ children }: { children: React.ReactNode }) {
 
 export default function Education() {
   return (
-    <section className="bg-black scroll-mt-20" id="education">
+    <section className="bg-[#08152F] scroll-mt-20" id="education">
       <div className="mx-auto max-w-6xl px-6 py-14">
         <div className="grid gap-10 md:grid-cols-12">
           {/* Left label */}
@@ -42,17 +42,17 @@ export default function Education() {
           <div className="md:col-span-9">
             <div className="space-y-12">
               {edcuation.map((edu) => (
-                <div key={edu.scholl_name} className="space-y-2">
+                <div key={edu.scholl_name} className="space-y-2 font-poppins font-semibold">
                   {/* Title + date */}
                   <div className="flex flex-col gap-2 sm:flex-row sm:items-baseline sm:justify-between">
                     <h3 className="text-lg font-semibold text-zinc-100">
                       {edu.scholl_name}
                     </h3>
-                    <p className="text-sm text-zinc-400">{edu.dateRange}</p>
+                    <p className="text-sm text-zinc-400 font-medium">{edu.dateRange}</p>
                   </div>
 
                   {/* Location */}
-                  <p className="text-sm font-medium text-sky-400">{edu.location}</p>
+                  <p className="text-sm font-medium text-white">{edu.location}</p>
 
                   {/* Location */}
                   <p className="text-sm font-medium text-sky-400">{edu.desc}</p>
