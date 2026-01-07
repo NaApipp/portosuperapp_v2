7d5b56985989d0b741f36acb16ac10132950822c
NabilApipp <nabilapipp@gmail.com>
2026-01-07 15:56:27 +0700

    create main page for project

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,7 +1,20 @@
+import { projectData } from "@/app/lib/projectData";
+import ProjectCard from "./components/projectCard";
+import Footer from "./components/footer";
+import Navbar from "./components/navbar";
+
 export default function Project() {
     return (
         <>
-        <p>Hello</p>
+        <Navbar/>
+        <main className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 pl-10 pr-10 pb-20 pt-20 bg-[#08152F]">
+            {projectData.map((p) => (
+                <ProjectCard key={p.id} project={p} />
+            ))}
+        </main>
+        <footer>
+            <Footer/>
+        </footer>
         </>
-    )
+    );
 }
