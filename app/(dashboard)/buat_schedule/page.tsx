00759d59173318cfbe4f2f8424d1e38ffc2c65d4
NabilApipp <nabilapipp@gmail.com>
2026-01-14 16:43:24 +0700

    add page read schedule

--- app/(dashboard)/buat_schedule/page.tsx
+++ app/(dashboard)/buat_schedule/page.tsx
@@ -1,12 +1,15 @@
 import AddSchdule from "./components/schduleForm";
 import Navbar from "../components/navbar";
+import ReadSchedule from "./components/readSchedule";
 
 export default function page() {
   return (
     <>
       <Navbar />
       <div className="grid grid-cols-2 min-h-screen">
-        <div className="bg-red-500" id="liat_schedule"></div>
+        <div className="flex justify-center items-center mt-9 bg-red-500" id="liat_schedule">
+            <ReadSchedule />
+        </div>
         <div className="flex justify-center items-center mt-9 bg-[#08152F]" id="add_scehdule">
             <AddSchdule />
         </div>
