ba8d99ca1cdd32f4bfeaf4797dd88d1eb9c549e7
NabilApipp <nabilapipp@gmail.com>
2026-02-01 10:29:55 +0700

    add metadata

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,15 @@
 // app/(protected)/layout.tsx
 import { AuthProvider } from "./authProvider";
 
+import type { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: 'SuperApps Dashboard',
+  description: 'Access Dashboard SuperApps (Private)',
+}
+
+
 export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
   return <AuthProvider>{children}</AuthProvider>;
 }
