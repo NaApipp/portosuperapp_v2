c74d5e94b4b43057baeaa62de0a3e90ca83a7ed7
NabilApipp <nabilapipp@gmail.com>
2026-02-01 10:31:12 +0700

    change message card

--- app/(dashboard)/dashboard/components/messages.tsx
+++ app/(dashboard)/dashboard/components/messages.tsx
@@ -38,13 +38,27 @@ export default function MessagesPage() {
   }, []);
 
   return (
-    <div className="grid grid-cols-3 gap-2">
+    <div className="container-message">
         {items.map((m) => (
-          <div key={m._id} className="bg-red-400 p-4 mb-4 rounded-md border border-red-200">
-            <p>Nama Pengirim: <span className="data">{m.email}</span></p>
-            <p>Pesan: <span className="data">{m.message}</span></p>
-            <p>Waktu: <span className="data">{m.message_date}</span></p>
-          </div>
+<div key={m._id} className="bg-gray-900 w-max h-64 rounded-lg p-3">
+    <div className="flex p-2 gap-1">
+    <div className="">
+      <span className="bg-red-500 inline-block center w-3 h-3 rounded-full"></span>
+    </div>
+    <div className="circle">
+      <span className="bg-yellow-500 inline-block center w-3 h-3 rounded-full"></span>
+    </div>
+    <div className="circle">
+      <span className="bg-green-500 box inline-block center w-3 h-3 rounded-full"></span>
+    </div>
+  </div>
+  <div className="card__content">
+    <p className="font-bold ">Nama : <span className="data-message">{m.name}</span></p>
+    <p className="font-bold ">Email: <span className="data-message">{m.email}</span></p>
+    <p className="font-bold ">Pesan: <span className="data-message">{m.message}</span></p>
+    <p className="font-bold ">Waktu: <span className="data-message">{m.message_date}</span></p>
+  </div>
+</div>
         ))}
       {/* {hasNext && (
         <button className="button-load text-white" onClick={() => loadMore(false)} disabled={loading}>
