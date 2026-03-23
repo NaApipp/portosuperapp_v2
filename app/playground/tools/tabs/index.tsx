9bbaaef4dbf868c5d2c1129f146d4cd1bd1ede02
NabilApipp <nabilapipp@gmail.com>
2026-03-23 12:14:53 +0700

    feat: create base page for playgrounf/tabs

---
+++ app/playground/tools/tabs/index.tsx
@@ -0,0 +1,38 @@
+// app/tools/Tabs.tsx
+"use client";
+
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { Keyboard, ChartCandlestick, MapPin } from "lucide-react";
+
+export default function Tabs() {
+  const searchParams = useSearchParams();
+  const activeTab = searchParams.get("tab") || "wpm";
+
+  const tabs = [
+    { icon: <Keyboard />,name: "WPM", value: "wpm" },
+    { icon: <MapPin />,name: "Postal Code", value: "postal-code" },
+    { icon: <ChartCandlestick />,name: "Investment", value: "investment" },
+  ];
+
+  return (
+    <div style={{ display: "flex", gap: 16 }}>
+      {tabs.map((tab) => (
+        <Link
+          key={tab.value}
+          href={`/playground/tools?tab=${tab.value}`}
+          style={{
+            fontWeight: activeTab === tab.value ? "bold" : "normal",
+          }}
+          className="w-full sm:w-auto inline-flex items-center justify-center gap-3 
+                    rounded-2xl dark:bg-white bg-[#08152F] px-8 py-4 text-sm font-bold
+                     dark:text-black text-white transition-all duration-300 
+                     hover:scale-[1.02] active:scale-[0.98] disabled:opacity-50 
+                     cursor-pointer shadow-lg hover:shadow-xl dark:shadow-none"
+        >
+          <span>{tab.icon}</span> <span className="font-black">{tab.name}</span>
+        </Link>
+      ))}
+    </div>
+  );
+}
