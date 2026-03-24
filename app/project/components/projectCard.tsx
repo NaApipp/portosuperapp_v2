64c33ca62d7668ae1c10021fcefc1f0cab1f25b2
NabilApipp <nabilapipp@gmail.com>
2026-03-24 19:10:32 +0700

    fix (edu, exp, projectCard): fixing (delete) manual animation

--- app/project/components/projectCard.tsx
+++ app/project/components/projectCard.tsx
@@ -7,8 +7,7 @@ export default function projectCard({ project }: { project: ProjectData }) {
 
   return (
     <>
-      <article className="flex flex-col h-full overflow-hidden rounded-2xl border border-zinc-200 dark:border-zinc-700 text-[#08152F] dark:text-white bg-white dark:bg-[#2383AD]/40 shadow-sm transition-all duration-500 ease-out hover:scale-[1.03] hover:shadow-xl dark:hover:shadow-none
-       opacity-0 translate-y-10 transition-all duration-700 ease-out scroll-animate">
+      <article className="flex flex-col h-full overflow-hidden rounded-2xl border border-zinc-200 dark:border-zinc-700 text-[#08152F] dark:text-white bg-white dark:bg-[#2383AD]/40 shadow-sm transition-all duration-500 ease-out hover:scale-[1.03] hover:shadow-xl dark:hover:shadow-none">
         <div className="relative aspect-video w-full bg-neutral-100 shrink-0">
           <Image
             src={project.image}
