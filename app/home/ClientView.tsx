745819292eb1f43b641a2e0de2a2430477413f07
NaApipp <nabilapipp@gmail.com>
2026-09-12 21:56:30 +0700

    feat: add new new components to path home

--- app/home/ClientView.tsx
+++ app/home/ClientView.tsx
@@ -1,12 +1,18 @@
 import Hero from "./components/hero";
+import HeroV3 from "./components/HeroV3";
 import Education from "./components/education";
+import EducationV3 from "./components/EducationV3";
 import Experience from "./components/experrience";
+import ExperienceV3 from "./components/experiencev3";
 import GithubStatus from "./components/GithubStats_Component/gitStatus";
 import TechStack from "./components/TechStack";
+import TechStackV3 from "./components/TechStackV3";
 import SeeProject from "./components/SeeProject";
+import SeeProjectV3 from "./components/SeeProjectV3";
 import Footer from "./components/footer";
 import { getGitHubContributions, getGitHubRepos, getGitHubStats } from "@/app/lib/github";
 import TypingDataUi from "./components/TypingDataUi";
+import Header from "@/app/components/navbar";
 
 export default async function ClientView() {
   const [stats, repos, contributions] = await Promise.all([
@@ -17,19 +23,27 @@ export default async function ClientView() {
 
   return (
     <>
-      <div className="dark:bg-[#08152F] bg-[#F8FAFC]">
+    <header className="cursor-default sticky top-0 z-50">
+      <Header/>
+    </header>
+      <div>
         <main>
-        <Hero/>
-        <Education/>
-        <Experience/>
-        <TechStack/>
+        {/* <Hero/> */}
+        <HeroV3/>
+        {/* <Education/> */}
+        <EducationV3/>
+        {/* <Experience/> */}
+        <ExperienceV3/>
+        {/* <TechStack/> */}
+        <TechStackV3/>
         <TypingDataUi />
         <GithubStatus 
           stats={stats} 
           repos={repos} 
           contributions={contributions} 
         />
-        <SeeProject />
+        {/* <SeeProject /> */}
+        <SeeProjectV3/>
         </main>
         <Footer/>
       </div>
